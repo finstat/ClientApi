02fd413eaaee77e8460906274b9aab1c39aa610d
Erik Horváth <erik@myself.sk>
2017-03-14 08:51:29 +0100

    metody pre pracu s exekuciami

--- FinstatApiNETClient/FinstatApi/ApiClient.cs
+++ FinstatApiNETClient/FinstatApi/ApiClient.cs
@@ -227,5 +227,289 @@ namespace FinstatApi
                 throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
             }
         }
+
+        /// <summary>
+        /// Requests the distraint results for specified search.
+        /// </summary>
+        /// <param name="ico"></param>
+        /// <param name="surname"></param>
+        /// <param name="dateOfBirth"></param>
+        /// <param name="city"></param>
+        /// <param name="companyName"></param>
+        /// <param name="fileReference"></param>
+        /// <param name="json"></param>
+        /// <returns>DistraintResult</returns>
+        /// <exception cref="FinstatApi.FinstatApiException">
+        /// Not valid API key!
+        /// or Url {0} not found!
+        /// or Unknown exception while communication with Finstat api!
+        /// </exception>
+        public DistraintResult RequestDistraintSearch(string ico, string surname, string dateOfBirth, string city, string companyName, string fileReference, bool json = false)
+        {
+            try
+            {
+                using (WebClient client = new WebClientWithTimeout(_timeout))
+                {
+                    var search = string.Format("{0}|{1}|{2}|{3}|{4}|{5}", ico, surname, dateOfBirth, city, companyName, fileReference);
+
+                    System.Collections.Specialized.NameValueCollection reqparm =
+                        new System.Collections.Specialized.NameValueCollection();
+                    reqparm.Add("search", search);
+                    reqparm.Add("apiKey", _apiKey);
+                    reqparm.Add("Hash", ComputeVerificationHash(_apiKey, _privateKey, search));
+                    reqparm.Add("StationId", _stationId);
+                    reqparm.Add("StationName", _stationName);
+                    byte[] responsebytes = client.UploadValues(_url + "/distraintSearch" + (json ? ".json" : null), "POST", reqparm);
+                    var response = Encoding.UTF8.GetString(responsebytes);
+                    using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(response))))
+                    {
+                        if (json)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return (DistraintResult)serializer.Deserialize(reader, typeof(DistraintResult));
+                        }
+                        else
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(DistraintResult));
+                            return (DistraintResult)serializer.Deserialize(reader);
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw ParseErrorResponse(e);
+            }
+            catch (Exception e)
+            {
+                throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
+            }
+        }
+
+        /// <summary>
+        /// Requests the detail results for specified token and list of detail ids.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="ids"></param>
+        /// <param name="json"></param>
+        /// <returns>DistraintDetailResult</returns>
+        /// <exception cref="FinstatApi.FinstatApiException">
+        /// Not valid API key!
+        /// or Url {0} not found!
+        /// or Unknown exception while communication with Finstat api!
+        /// </exception>
+        public DistraintDetailResults RequestDistraintDetail(string token, int[] ids, bool json = false)
+        {
+            var idsString = String.Empty;
+            var idsParam = String.Empty;
+            if (ids != null)
+            {
+                foreach (var id in ids)
+                {
+                    idsString += id;
+                    idsParam += (!string.IsNullOrEmpty(idsParam) ? "," : null) + id;
+                }
+            }   
+
+            try
+            {
+                using (WebClient client = new WebClientWithTimeout(_timeout))
+                {
+                    System.Collections.Specialized.NameValueCollection reqparm =
+                        new System.Collections.Specialized.NameValueCollection();
+                    reqparm.Add("token", token);
+                    reqparm.Add("ids", idsParam);
+                    reqparm.Add("apiKey", _apiKey);
+                    reqparm.Add("Hash", ComputeVerificationHash(_apiKey, _privateKey, token + idsString));
+                    reqparm.Add("StationId", _stationId);
+                    reqparm.Add("StationName", _stationName);
+                    byte[] responsebytes = client.UploadValues(_url + "/distraintDetail" + (json ? ".json" : null), "POST", reqparm);
+                    var response = Encoding.UTF8.GetString(responsebytes);
+                    using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(response))))
+                    {
+                        if (json)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return (DistraintDetailResults)serializer.Deserialize(reader, typeof(DistraintDetailResults));
+                        }
+                        else
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(DistraintDetailResults));
+                            return (DistraintDetailResults)serializer.Deserialize(reader);
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw ParseErrorResponse(e);
+            }
+            catch (Exception e)
+            {
+                throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
+            }
+        }
+
+        /// <summary>
+        /// Requests the distraint results for specified search.
+        /// </summary>
+        /// <param name="ico"></param>
+        /// <param name="surname"></param>
+        /// <param name="dateOfBirth"></param>
+        /// <param name="city"></param>
+        /// <param name="companyName"></param>
+        /// <param name="fileReference"></param>
+        /// <param name="json"></param>
+        /// <returns>DistraintResult</returns>
+        /// <exception cref="FinstatApi.FinstatApiException">
+        /// Not valid API key!
+        /// or Url {0} not found!
+        /// or Unknown exception while communication with Finstat api!
+        /// </exception>
+        public DistraintResult RequestDistraintResults(string ico, string surname, string dateOfBirth, string city, string companyName, string fileReference, bool json = false)
+        {
+            try
+            {
+                using (WebClient client = new WebClientWithTimeout(_timeout))
+                {
+                    var search = string.Format("{0}|{1}|{2}|{3}|{4}|{5}", ico, surname, dateOfBirth, city, companyName, fileReference);
+
+                    System.Collections.Specialized.NameValueCollection reqparm =
+                        new System.Collections.Specialized.NameValueCollection();
+                    reqparm.Add("search", search);
+                    reqparm.Add("apiKey", _apiKey);
+                    reqparm.Add("Hash", ComputeVerificationHash(_apiKey, _privateKey, search));
+                    reqparm.Add("StationId", _stationId);
+                    reqparm.Add("StationName", _stationName);
+                    byte[] responsebytes = client.UploadValues(_url + "/distraintResults" + (json ? ".json" : null), "POST", reqparm);
+                    var response = Encoding.UTF8.GetString(responsebytes);
+                    using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(response))))
+                    {
+                        if (json)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return (DistraintResult)serializer.Deserialize(reader, typeof(DistraintResult));
+                        }
+                        else
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(DistraintResult));
+                            return (DistraintResult)serializer.Deserialize(reader);
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw ParseErrorResponse(e);
+            }
+            catch (Exception e)
+            {
+                throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
+            }
+        }
+
+        /// <summary>
+        /// Requests the distraint results for specified search.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="fileReference"></param>
+        /// <param name="json"></param>
+        /// <returns>DistraintResult</returns>
+        /// <exception cref="FinstatApi.FinstatApiException">
+        /// Not valid API key!
+        /// or Url {0} not found!
+        /// or Unknown exception while communication with Finstat api!
+        /// </exception>
+        public DistraintResult RequestDistraintResultsByToken(string token, bool json = false)
+        {
+            try
+            {
+                using (WebClient client = new WebClientWithTimeout(_timeout))
+                {
+                    System.Collections.Specialized.NameValueCollection reqparm =
+                        new System.Collections.Specialized.NameValueCollection();
+                    reqparm.Add("token", token);
+                    reqparm.Add("apiKey", _apiKey);
+                    reqparm.Add("Hash", ComputeVerificationHash(_apiKey, _privateKey, token));
+                    reqparm.Add("StationId", _stationId);
+                    reqparm.Add("StationName", _stationName);
+                    byte[] responsebytes = client.UploadValues(_url + "/distraintResultsByToken" + (json ? ".json" : null), "POST", reqparm);
+                    var response = Encoding.UTF8.GetString(responsebytes);
+                    using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(response))))
+                    {
+                        if (json)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return (DistraintResult)serializer.Deserialize(reader, typeof(DistraintResult));
+                        }
+                        else
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(DistraintResult));
+                            return (DistraintResult)serializer.Deserialize(reader);
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw ParseErrorResponse(e);
+            }
+            catch (Exception e)
+            {
+                throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
+            }
+        }
+
+        /// <summary>
+        /// Requests the stored detail results for specified id.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="ids"></param>
+        /// <param name="json"></param>
+        /// <returns>DistraintDetailResult</returns>
+        /// <exception cref="FinstatApi.FinstatApiException">
+        /// Not valid API key!
+        /// or Url {0} not found!
+        /// or Unknown exception while communication with Finstat api!
+        /// </exception>
+        public DistraintDetailResults RequestDistraintStoredDetail(string id, bool json = false)
+        {
+            try
+            {
+                using (WebClient client = new WebClientWithTimeout(_timeout))
+                {
+                    System.Collections.Specialized.NameValueCollection reqparm =
+                        new System.Collections.Specialized.NameValueCollection();
+                    reqparm.Add("id", id);
+                    reqparm.Add("apiKey", _apiKey);
+                    reqparm.Add("Hash", ComputeVerificationHash(_apiKey, _privateKey, id));
+                    reqparm.Add("StationId", _stationId);
+                    reqparm.Add("StationName", _stationName);
+                    byte[] responsebytes = client.UploadValues(_url + "/distraintStoredDetail" + (json ? ".json" : null), "POST", reqparm);
+                    var response = Encoding.UTF8.GetString(responsebytes);
+                    using (var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(response))))
+                    {
+                        if (json)
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            return (DistraintDetailResults)serializer.Deserialize(reader, typeof(DistraintDetailResults));
+                        }
+                        else
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(DistraintDetailResults));
+                            return (DistraintDetailResults)serializer.Deserialize(reader);
+                        }
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                throw ParseErrorResponse(e);
+            }
+            catch (Exception e)
+            {
+                throw new FinstatApiException(FinstatApiException.FailTypeEnum.Unknown, "Unknown exception while processing Finstat api request!", e);
+            }
+        }
     }
 }
---
+++ FinstatApiNETClient/FinstatApi/DistraintResult.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinstatApi
+{
+    public class DistraintResult
+    {
+        public int Count { get; set; }
+        public DistraintPreview[] Distraints { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            result.AppendLine(string.Format("Count: {0}", Count));
+            if (Distraints != null && Distraints.Length > 0)
+            {
+                result.AppendLine("\nDistraints:");
+                foreach (var distraint in Distraints)
+                {
+                    result.AppendLine(distraint.ToString());
+                }
+            }
+            return result.ToString();
+        }
+    }
+
+    public class DistraintDetailResults
+    {
+        public DistraintDetailResult[] DistraintDetails { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            if (DistraintDetails != null && DistraintDetails.Length > 0)
+            {
+                result.AppendLine("\nDistraints:");
+                foreach (var distraint in DistraintDetails)
+                {
+                    result.AppendLine(distraint.ToString());
+                }
+            }
+            return result.ToString();
+        }
+    }
+
+    public class DistraintPreview
+    {
+        public string Code { get; set; }        // Znacka
+        public Debtor[] Debtors;              // Povinni
+        public int TypeOfAuthorisation { get; set; }   // TypPoverenia
+        public DateTime Created { get; set; }   
+        public int DetailId { get; set; }
+        public string DetailToken { get; set; }
+        public string StoredDetailId { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            result.AppendLine(string.Format("Code: {0}", Code));
+            if (Debtors != null && Debtors.Length > 0)
+            {
+                result.AppendLine("\nDebtors:");
+                foreach (var oblig in Debtors)
+                {
+                    result.AppendLine(oblig.ToString());
+                }
+            }
+            result.AppendLine(string.Format("TypeOfAuthorisation: {0} Created: {1} DetailId {2} DetailToken {3} StoredDetailId {4}", 
+                TypeOfAuthorisation, Created, DetailId, DetailToken, StoredDetailId));
+            return result.ToString();
+        }
+    }
+
+    public class DistraintDetailResult : DistraintPreview
+    {
+        public string Court { get; set; }               // Sud
+        public DateTime DateOfAuthorisation { get; set; }      // DatumPoverenia
+        public string CourtCode { get; set; }           // SudZn
+        public string EnforcementDetails { get; set; }    // PopisNaroku
+        public decimal SumOutstanding { get; set; }             // VyskaPlnenia
+        public string Currency { get; set; }            // Mena
+        public Bailiff Bailiff;                       // Exekutor
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            base.ToString();
+            result.AppendLine(string.Format("Court: {0} DateOfAuthorisation: {1} CourtCode: {2} SumOutstanding: {3} Currency: {4}", 
+                Court, DateOfAuthorisation, CourtCode, SumOutstanding, Currency));
+            result.AppendLine(Bailiff.ToString());
+            result.AppendLine(string.Format("ClaimDescription: {0}",
+                EnforcementDetails));
+            return result.ToString();
+        }
+    }
+
+    public class Debtor
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdentificationNumber { get; set; }    // Rodne cislo
+        public string ICO { get; set; }
+        public string CompanyName { get; set; }
+        public string AddressType { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZIP { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            result.AppendLine(string.Format("Type: {0} Name: {1} Surname: {2} DateOfBirth: {3} IdentificationNumber: {4} ICO: {5} CompanyName: {6} AddressType: {7} Street: {8} City: {9} ZIP: {10}",
+                Type, Name, Surname, DateOfBirth, IdentificationNumber, ICO, CompanyName, AddressType, Street, City, ZIP));
+            return result.ToString();
+        }
+    }
+
+    public class Bailiff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string ZIP { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine(base.ToString());
+
+            result.AppendLine(string.Format("Id: {0} Name: {1} Street: {2} ZIP: {3} City: {4}",
+                Id, Name, Street, ZIP, City));
+            return result.ToString();
+        }
+    }
+}
