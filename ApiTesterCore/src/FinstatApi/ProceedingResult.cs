61d44a359b6b4e794f9bfd285fda5df830dfe720
František Bereň <birko@live.com>
2017-11-02 15:02:06 +0100

    api fields rename for proceedings

--- ApiTesterCore/src/FinstatApi/ProceedingResult.cs
+++ ApiTesterCore/src/FinstatApi/ProceedingResult.cs
@@ -21,12 +21,13 @@ namespace FinstatApi
 
     public class ProceedingResult
     {
-        public PersonAddress[] Defendant { get; set; }
-        public PersonAddress[] Proposer { get; set; }
-        public PersonAddress[] Administrator { get; set; }
-        public FullAddress Court { get; set; }
+        public PersonAddress[] DebtorsAddress { get; set; }
+        public PersonAddress[] ProposersAddress { get; set; }
+        public PersonAddress[] AdministratorsAddress { get; set; }
+        public FullAddress CourtsAddress { get; set; }
         public string ReferenceFileNumber { get; set; }
-        public string State { get; set; }
+        public string Status { get; set; }
+        public string Character { get; set; }
         public string EndReason { get; set; }
         public string Url { get; set; }
         public string Type { get; set; }
@@ -35,11 +36,13 @@ namespace FinstatApi
         public override string ToString()
         {
             StringBuilder dataString = new StringBuilder();
-            dataString.AppendLine(string.Format("Defendant Count: {0}", Defendant != null ? Defendant.Length : 0));
-            dataString.AppendLine(string.Format("Proposer Count: {0}", Proposer != null ? Proposer.Length : 0));
-            dataString.AppendLine(string.Format("Administrator Count: {0}", Administrator != null ? Administrator.Length : 0));
-            dataString.AppendLine(string.Format("Court: {0}", Court));
+            dataString.AppendLine(string.Format("DebtorsAddress Count:{0}", DebtorsAddress != null ? DebtorsAddress.Length : 0));
+            dataString.AppendLine(string.Format("ProposersAddress: {0}", ProposersAddress != null ? ProposersAddress.Length : 0));
+            dataString.AppendLine(string.Format("AdministratorsAddress Count: {0}", AdministratorsAddress != null ? AdministratorsAddress.Length : 0));
+            dataString.AppendLine(string.Format("CourtsAddress: {0}", CourtsAddress));
             dataString.AppendLine(string.Format("EndReason: {0}", EndReason));
+            dataString.AppendLine(string.Format("Status: {0}", Status));
+            dataString.AppendLine(string.Format("Character: {0}", Character));
             dataString.AppendLine(string.Format("Url: {0}", Url));
             dataString.AppendLine(string.Format("Type: {0}", Type));
             dataString.AppendLine(string.Format("PublishDate: {0}", PublishDate));
