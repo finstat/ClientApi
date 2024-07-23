e976670e8cf38727354d0fdebbb4ed37ef8fbdb9
František Bereň <birko@live.com>
2024-07-23 14:11:46 +0200

    updated project references

--- Tester/CZ.Core/ApiCzTesterCore/Program.cs
+++ Tester/CZ.Core/ApiCzTesterCore/Program.cs
@@ -1,10 +1,7 @@
 ﻿using FinstatApi;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace ApiMonitoringTester
 {
--- Tester/CZ.Core/ApiMonitoringTester/Program.cs
+++ Tester/CZ.Core/ApiMonitoringTester/Program.cs
@@ -1,9 +1,6 @@
 ﻿using FinstatApi;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiCzTesterCore
 {
